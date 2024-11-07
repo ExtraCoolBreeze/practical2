24a173452c66cb18907fcaccdcc2c7635059009d
“ExtraCoolBreeze” <craigmcmillan@live.co.uk>
2024-11-07 01:34:57 +0000

    added Dec2HexTest.javva back in after undoing commit

---
+++ Dec2HexTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Dec2HexTest {
+
+    @Test
+    public void testNormal() {
+        assertEquals("10 should be A", "A", Dec2Hex.decimalToHexadecimal(10));
+        assertEquals("255 should be FF", "FF", Dec2Hex.decimalToHexadecimal(255));
+        assertEquals("0 should be 0", "0", Dec2Hex.decimalToHexadecimal(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionalNegative() {
+        Dec2Hex.decimalToHexadecimal(-1);  // This should throw IllegalArgumentException for negative input
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionalInvalidInput() {
+        Dec2Hex.main(new String[]{});  // This should throw IllegalArgumentException for no input argument
+    }
+
+    @Test
+    public void testExtreme() {
+        assertEquals("The hexadecimal of 1023 should be 3FF", "3FF", Dec2Hex.decimalToHexadecimal(1023));
+        assertEquals("The hexadecimal of 2047 should be 7FF", "7FF", Dec2Hex.decimalToHexadecimal(2047));
+        assertEquals("The hexadecimal of 65535 should be FFFF", "FFFF", Dec2Hex.decimalToHexadecimal(65535));
+    }
+}
